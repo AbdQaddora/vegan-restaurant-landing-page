5644a3088938904675390535b9d057619bc9fa67
AbdQaddora <abdqaddora732001@outlook.com>
2022-12-18 11:45:11 +0200

    newsletter responsive

--- src/sections/Newsletter/index.jsx
+++ src/sections/Newsletter/index.jsx
@@ -6,15 +6,18 @@ import email from '../../assets/images/icons/email2.png'
 
 import './style.css';
 import Button from '../../components/Button';
+import GridWithThreeColumns from '../../components/GridWithThreeColumns';
 const Newsletter = () => {
     return (
         <Container>
             <div className='newsletter'>
-                <p>
-                    <img src={email} alt="email" /> Subscribe to our Newsletter
-                </p>
-                <input type="text" placeholder='Your Email id' />
-                <Button>Subscribe</Button>
+                <GridWithThreeColumns>
+                    <p>
+                        <img src={email} alt="email" /> Subscribe to our Newsletter
+                    </p>
+                    <input type="text" placeholder='Your Email id' />
+                    <Button>Subscribe</Button>
+                </GridWithThreeColumns>
             </div>
         </Container>
     )
